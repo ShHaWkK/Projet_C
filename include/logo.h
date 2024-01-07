fdeb5542c75b290f168f05de5b041f34a7f4e534
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-07 03:11:26 +0100

    ADD logo.c and logo.h

---
+++ include/logo.h
@@ -0,0 +1,10 @@
+// Protection contre les inclusions multiples
+#ifndef LOGO_H
+#define LOGO_H
+
+#include "include.h"
+
+// Déclaration de la fonction pour charger et définir l'icône de la fenêtre
+void SetWindowIcon(SDL_Window* window, const char* filepath);
+
+#endif // LOGO_H
