08648c535584cffa76904fd201cf5b9f67d00bc8
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-01-09 01:24:46 +0100

    Update map.h preparation base

--- include/map.h
+++ include/map.h
@@ -1,8 +1,10 @@
 #include "include.h"
 
+#define SizeBlock // a definir
 
 typedef struct {
     SDL_Rect area;
     SDL_Texture* texture;
-    
+    enum{TOP,BOT,LEFT,RIGHT};//mouvement joueur
+    enum{};//les texture de chaque blocks
 } MapZone;
