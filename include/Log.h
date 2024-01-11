b20bc73ccc8107e4f65a48574381f69c897002e2
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-11 01:34:36 +0100

    Update Log.h

--- include/Log.h
+++ include/Log.h
@@ -2,6 +2,7 @@
 #define LOG_H
 
 #include "include.h"
+#include <time.h>
 
 typedef enum {
     LOG_INFO,
@@ -9,12 +10,16 @@ typedef enum {
     LOG_ERROR
 } LogLevel;
 
+// Initializes the logging system.
 void Log_Init(const char* logFileName);
+
+// Logs a message at the given log level.
 void Log(LogLevel level, const char* format, ...);
+
+// Closes the logging system.
 void Log_Close();
 
-
+// Sets the current log level. Messages below this level will not be logged.
 void Log_SetLevel(LogLevel level);
-extern LogLevel currentLogLevel;
 
 #endif // LOG_H
