313b7293496d1b8f917ede2374e48e5a48f7893f
ShHawk <alexandreuzan9@gmail.com>
2024-01-14 01:47:32 +0100

    Update (ADD include.h in database) + Add File Survivor.h and survivor.c

--- include/database.h
+++ include/database.h
@@ -1,10 +1,11 @@
-// database.h
+// File : database.h
 
 
 #ifndef DATABASE_H
 #define DATABASE_H
 
 #include "include.h"
+#include "survivor.h"
 
 int db_open(const char *filename, sqlite3 **db);
 int db_close(sqlite3 *db);
@@ -12,4 +13,5 @@ int db_execute(sqlite3 *db, const char *sql);
 int db_begin_transaction(sqlite3 *db);
 int db_commit_transaction(sqlite3 *db);
 int db_rollback_transaction(sqlite3 *db);
+
 #endif // DATABASE_H
