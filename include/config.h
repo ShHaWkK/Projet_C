ab7186ee2ef5356ba86cf05a7207867ce27e63c8
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-17 10:35:09 +0100

    Update config.h (Resolution screen) loading ..

--- include/config.h
+++ include/config.h
@@ -15,10 +15,16 @@ typedef struct {
     int (*supportedResolutions)[2];
 } GameConfig;
 
+//  ------------     Functions      ------------   //
+
+void initGameConfig(GameConfig *config);
 GameConfig loadConfig(const char *filename);
 void saveConfig(const GameConfig *config, const char *filename);
 void updateResolution(ScreenManager *screenManager, GameConfig *config);
 void renderConfigOptions(SDL_Renderer *renderer, TTF_Font *font, GameConfig *config);
 void handleConfigurations(ScreenManager *screenManager);
+void selectResolution(GameConfig *config, int index);
+void toggleFullScreen(GameConfig *config);
+void saveCurrentConfig(const GameConfig *config);
 
 #endif // CONFIG_H
