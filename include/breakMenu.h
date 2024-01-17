f2ef4d346144b33b78644a2789b68fa755361c43
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-01-18 00:58:28 +0100

    Add breakMenu.h

---
+++ include/breakMenu.h
@@ -0,0 +1,11 @@
+//
+// Created by alexa on 17/01/2024.
+//
+
+#ifndef C_BREAKMENU_H
+#define C_BREAKMENU_H
+#include "../include/include.h"
+
+void breakMenu(SDL_Renderer *renderer);
+
+#endif //C_BREAKMENU_H
