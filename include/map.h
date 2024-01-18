ff8e79c774a504f63b0f6faa52276e1d646540b3
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-18 04:18:42 +0100

    Update map.h (Add montaintexture )

--- include/map.h
+++ include/map.h
@@ -49,12 +49,17 @@ void freePlayerTexture();
 #define GROUND_HEIGHT (WINDOW_HEIGHT - SKY_HEIGHT)
 #define MOUNTAIN_WIDTH 200
 #define ENTRANCE_WIDTH 100
+#define MOUNTAIN_HEIGHT 300
+#define ENTRANCE_HEIGHT 100
 
 // Couleurs pour chaque zone
 #define COLOR_SKY (SDL_Color){135, 206, 235, 255} // Couleur bleu ciel
 #define COLOR_GROUND (SDL_Color){244, 164, 96, 255} // Couleur sable
 #define COLOR_MOUNTAIN (SDL_Color){139, 69, 19, 255} // Couleur marron pour la montagne
 #define COLOR_ENTRANCE (SDL_Color){160, 82, 45, 255} // Couleur plus sombre pour l'entrée
+
+
+static SDL_Texture* mountainTexture = NULL;
 
 typedef struct {
     SDL_Rect sky;
@@ -81,10 +86,12 @@ typedef struct {
 } Player;
 
 // Fonctions de gestion de la carte
-void initGameMap(GameMap* map);
+void initGameMap(GameMap* map, SDL_Renderer* renderer, SDL_Texture* mountainTexture);
 void renderGameMap(GameMap* map, SDL_Renderer* renderer);
 void freeGameMap(GameMap* map);
 void initMap(GameMap* map, SDL_Renderer* renderer);
 void renderMap(GameMap* map, SDL_Renderer* renderer);
 void freeMapTextures();
+void loadMountainTexture(SDL_Renderer* renderer);
+void freeGameMapResources();
 #endif // MAP_H
