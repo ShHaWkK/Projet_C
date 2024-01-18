4a13eec8f160f0a2546e94373c2dc8b732c11165
ShHawk <alexandreuzan9@gmail.com>
2024-01-18 19:10:51 +0100

    Update map.h

--- include/map.h
+++ include/map.h
@@ -40,23 +40,19 @@ void freePlayerTexture();
 #ifndef MAP_H
 #define MAP_H
 
-#include <SDL.h>
+#include "include.h"
+
 
 // Constantes pour les dimensions de la fenêtre et les zones de la carte
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define SKY_HEIGHT 150
-#define GROUND_HEIGHT (WINDOW_HEIGHT - SKY_HEIGHT)
-#define MOUNTAIN_WIDTH 160
-#define ENTRANCE_WIDTH 100
+#define SKY_HEIGHT 200
+#define GROUND_HEIGHT 100
+#define MOUNTAIN_WIDTH 300
 #define MOUNTAIN_HEIGHT 150
+#define ENTRANCE_WIDTH 50
 #define ENTRANCE_HEIGHT 100
 
-// Couleurs pour chaque zone
-#define COLOR_SKY (SDL_Color){135, 206, 235, 255} // Couleur bleu ciel
-#define COLOR_GROUND (SDL_Color){244, 164, 96, 255} // Couleur sable
-#define COLOR_MOUNTAIN (SDL_Color){139, 69, 19, 255} // Couleur marron pour la montagne
-#define COLOR_ENTRANCE (SDL_Color){160, 82, 45, 255} // Couleur plus sombre pour l'entrée
 
 
 static SDL_Texture* mountainTexture = NULL;
